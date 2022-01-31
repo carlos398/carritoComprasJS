d7a19541d5bfc32335c24ebf8ee30a13a6b4470f
Carlos Reyes <carlosarp2000@gmail.com>
2022-01-31 18:08:48 -0500

    capacidad de borrar frutas agregada

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-
+let id = 0;
 function app() {
     const btnagregarFruta = document.getElementById('agregarFruta');
     btnagregarFruta.addEventListener('click', agregarElemento);
+
+    getFruits()
 }
 
 function agregarElemento(e){
     e.preventDefault()
     let fruitName = document.getElementById('fruitName').value;
     let fruitPrice = document.getElementById('fruitPrice').value;
-    
+    id = id + 1;
     const fruta = {
         fruitName,
-        fruitPrice
+        fruitPrice,
+        id
     };
     saveFruit(fruta)
 }
@@ -41,12 +44,13 @@ function getFruits(){
     for(let i = 0; i < frutas.length; i++){
         let fruitName = frutas[i].fruitName;
         let fruitPrice = frutas[i].fruitPrice;
+        let fruitId = frutas[i].id;
         
         fruitsView.innerHTML += `<div class="card">
             <div class=card-body>
                 <div class="card-content">
                     <p>${fruitName} - price $ ${fruitPrice}</p>
-                    <a class="btn_delete">Borrar Fruta</a>
+                    <a class="btn_delete" onclick="sellFruit('${fruitId}')">Borrar Fruta</a>
                 </div>
                 <div class=card-img>
                     <img src="/imgs/${fruitName}.png" alt="">
@@ -57,5 +61,15 @@ function getFruits(){
     }
 }
 
+function sellFruit(fruitId){
+    let frutas = JSON.parse(localStorage.getItem('FRUTAS'))
+    for(let i = 0; i < frutas.length; i++){
+        if(frutas[i].id == fruitId){
+            frutas.splice(i, 1);
+        }
+    }
+    localStorage.setItem('FRUTAS', JSON.stringify(frutas));
+    getFruits()
+}
+
 app()
-getFruits()
